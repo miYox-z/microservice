40f5a21518bb2ce6847d0c41efff3af0eb9fd89a
zhuxuelin <1058852474@qq.com>
2019-03-29 23:10:28 +0800

    student controller

---
+++ msa2/calendarapi_30005/src/main/java/com/honestyzhang/msa/calendarapi_30005/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.honestyzhang.msa.calendarapi_30005.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: zhuxuelin
+ * @CreateTime: 2019-03-29
+ * @Description: TODO
+ */
+@RestController
+@RequestMapping("student")
+public class StudentController {
+}
