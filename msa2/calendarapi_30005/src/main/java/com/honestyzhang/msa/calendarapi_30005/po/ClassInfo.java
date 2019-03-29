5f201735b568c85518ac8deea0c586b3d8e6e742
guoxczz <guowang456@126.com>
2019-03-29 22:13:27 +0800

    提交班级po

---
+++ msa2/calendarapi_30005/src/main/java/com/honestyzhang/msa/calendarapi_30005/po/ClassInfo.java
@@ -0,0 +1,61 @@
+package com.honestyzhang.msa.calendarapi_30005.po;
+
+import java.sql.Timestamp;
+
+public class ClassInfo {
+
+    private long classId;
+    private String className;
+    private String classMaster;
+    private String classDesc ;
+    private Timestamp createTime;
+    private String state ;
+
+    public long getClassId() {
+        return classId;
+    }
+
+    public void setClassId(long classId) {
+        this.classId = classId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getClassMaster() {
+        return classMaster;
+    }
+
+    public void setClassMaster(String classMaster) {
+        this.classMaster = classMaster;
+    }
+
+    public String getClassDesc() {
+        return classDesc;
+    }
+
+    public void setClassDesc(String classDesc) {
+        this.classDesc = classDesc;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
